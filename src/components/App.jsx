6791e5adde5368cac0572012c80e33d2f6863fcd
Bruno Gomes <brunogomes1198@gmail.com>
2023-03-06 18:09:57 +1100

    Create components and App.jsx done

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Footer from "./Footer"
+import Note from "./Note"
+import Header from "./Header"
+
+function app() {
+    return(
+        <div>
+            <Header />
+            <Footer />
+            <Note />
+        </div>
+    )
+}
+    
+
+
+export default App 
